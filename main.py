ec860d97c4377abe611e430b2c8d572d9551c199
Pulecz <pulec.mcm@gmail.com>
2017-01-12 20:56:49 +0100

    First lines

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import urllib.request
+"""
+V0.1 GET yt subtitles
+example API url to get subtitles
+https://www.youtube.com/api/timedtext?lang=en&v=3NxKH1MK4z8&fmt=vtt&name=
+"""
+
+test_ids = [
+'3NxKH1MK4z8' #Great War week 129
+]
+
+def get_yt_sub_url(id):
+    """
+    return API url
+    TODO add format option
+    """
+    url = 'https://www.youtube.com/api/timedtext?lang=en&v=' + id +'&fmt=vtt&name='
+    return url
+
+def get_subtitles(input_video):
+    """
+    return data of url
+    TODO better return
+    """
+    try:
+        get = urllib.request.urlopen(input_video)
+        data = get.read()
+        if len(data) > 0:
+            return data
+        else:
+            return False # no subtitles
+    except ValueError:
+        print('Unkown url',input_video)
+
+print(get_subtitles(get_yt_sub_url(test_ids[0])))
