d940b29a8c660d7fbd86b26ac034cf2acb883f5a
Chris Davies <chris.greentown@gmail.com>
2017-01-21 00:34:33 -0500

    trying to make the db's schema updatable, still errors

--- CONST.py
+++ CONST.py
@@ -1,4 +1,4 @@
-db_name = 'test.db'
+db_name = 'vidinfo.db'
 caption_patt = '[\w\s\d\\\,\.\;\:\$\!\%\)\(\?\/\'\"\-]+'
 timestamp_patt = '\d\d:\d\d:\d\d\.\d\d\d\s-->\s\d\d:\d\d:\d\d\.\d\d\d'
 synctag_patt = '<\d+>'
--- db/DbTools.py
+++ db/DbTools.py
@@ -10,19 +10,37 @@ Provides the low-level functions to insert, query and update the db
 def init():
     con = sqlite3.connect( CONST.db_name )
     # asr value is auto-speech-recognition rendered captions, either 0 (false) or 1 (true)
+    #con.execute( "CREATE TABLE IF NOT EXISTS schemaversion
+    #              ( version real )" )
+    try:
+        cursor = con.execute( "SELECT version FROM schemaversion WHERE rowid = 1" )
+        version = cursor.fetchone()[0]
+    except sqlite3.OperationalError as e:
+        if ( e.args[0] == 'no such table: schemaversion' ):
+            con.execute( '''CREATE TABLE IF NOT EXISTS schemaversion
+                          ( rowid INTEGER PRIMARY KEY, version text)''' )
+            version = "0.0"
+            insert( version, table='schemaversion' )
     con.execute( '''CREATE TABLE IF NOT EXISTS subtitles
-                    ( urlid text, title text, captions text, timestamps text, asr integer, ROWID INTEGER PRIMARY KEY )''' )
+                    ( rowid INTEGER PRIMARY KEY, urlid text, title text, captions text, timestamps text, asr integer )''' )
+    print( 'initial database version = ', version )
+    if version == "0.0":
+        con.execute( '''ALTER TABLE subtitles
+                        ADD version TEXT;''' )
+        con.execute( '''UPDATE schemaversion
+                        SET version = '0.1';''' )
     return con
 
 def insert( *args,  table='subtitles', **kwargs ):
     """
-    Takes 3 arguments in the following order: String video_title, String url_id, String subtitles
+    Inserts a complete row into a table, the rowid is automatically appended as NULL to the end
     """
     con = init()
+    args_str = ','.join( args )
 
     try:
         with con:
-            con.execute( "INSERT INTO " + table + " VALUES ( '" + args[0] + "', '" + args[1] + "', '" + args[2] + "', '" + args[3] + "', '" + args[4] + "', NULL )" )
+            con.execute( "INSERT INTO " + table + " VALUES ( 'NULL, " + args_str + "')" )
     except sqlite3.IntegrityError:
         print( "Error inserting into db" )
 
@@ -55,6 +73,9 @@ def get_column_from_urlid( urlid, column ):
     return get_column_from_rowid( get_rowid_from_urlid( urlid ), column )
 
 def parse_subtitles( subtitles ):
+    """
+    I don't think this belongs in DbTools
+    """
     # match[0] is timestamp, [1] is caption
     matches = re.findall( r'(\d\d:\d\d:\d\d\.\d\d\d\s-->\s\d\d:\d\d:\d\d\.\d\d\d)\\n([\w\s\d\\\,\.\;\:\$\!\%\)\(\?\/\'\"\-]+)\\n\\n', subtitles ) 
     captions = ""
