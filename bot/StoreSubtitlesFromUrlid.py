116635706f157c91dff720729d886872aec1701d
Pulecz <pulec.mcm@gmail.com>
2017-01-22 20:29:25 +0100

    V0.2 get_metadata added and editor store

--- bot/StoreSubtitlesFromUrlid.py
+++ bot/StoreSubtitlesFromUrlid.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python
 import sys
 sys.path = ['.', '..'] + sys.path
-import re
-from db import DbTools
-import urllib.request
+import re #for def store()
+from db import DbTools #for def store()
+from urllib.request import urlopen #to get subtitles
+from pafy import new #for getting metadata
 
 """
 V0.1 GET yt subtitles
+V0.2 GET video metadata using pafy
 example API url to get subtitles
 https://www.youtube.com/api/timedtext?lang=en&v=3NxKH1MK4z8&fmt=vtt&name=
 """
 
 test_ids = [
-'3NxKH1MK4z8' #Great War week 129
+'3NxKH1MK4z8' #Great War week 129 - regular subtitles, multilang
 ]
 
 def get_yt_sub_url(id):
-    """
-    return API url
-    TODO add format option
-    """
+    """return yt API url to download subtitles in vtt format"""
     url = 'https://www.youtube.com/api/timedtext?lang=en&v=' + id +'&fmt=vtt&name='
     return url
 
 def get_raw_subtitles(input_video):
     """
-    return data of url
-    TODO better return
+    return data from input_video url
+    TODO better error handling
     """
     try:
-        get = urllib.request.urlopen(input_video)
+        get = urlopen(input_video)
         data = get.read()
         if len(data) > 0:
             return data
@@ -37,24 +36,30 @@ def get_raw_subtitles(input_video):
             return False # no subtitles
     except ValueError:
         print('Unkown url',input_video)
+        # TODO return error
 
-def get_title( url_or_urlid ):
-    """
-    returns title from url or urlid
-    """
-    return ""
+def get_metadata(video_id):
+    try:
+        url = 'https://www.youtube.com/watch?v=' + video_id
+        return new(url)
+    except ValueError:
+        print('Unkown url',url)
+        # TODO return error
 
-def get_author( url_or_urlid ):
-    return ""
-def get_length( url_or_urlid ):
-    return ""
-def get_date( url_or_urlid ):
-    return ""
-def get_category( url_or_urlid ):
-    return ""
-def get_tags( url_or_urlid ):
-    return ""
 
+def get_title(video):
+    return video.title
+def get_author(video):
+    return video.author
+def get_length(video):
+    #TODO must be str because of args_str = "','".join( args ) in DbTools.insert_args
+    return str(video.length)
+def get_date(video):
+    return video.published
+def get_category(video):
+    return video.category
+def get_tags(video):
+    return ','.join(video.keywords)
 
 
 def store( url_or_urlid ):
@@ -67,21 +72,16 @@ def store( url_or_urlid ):
     if ( url_validate_test ):
         pass
     elif ( urlid_validate_test ):
+        metadata = get_metadata(url_or_urlid)
         DbTools.insert( url_or_urlid,\
                         get_raw_subtitles( get_yt_sub_url( url_or_urlid ) ),\
-                        get_title( url_or_urlid ),\
-                        get_author( url_or_urlid ),\
-                        get_length( url_or_urlid ),\
-                        get_date( url_or_urlid ),\
-                        get_category( url_or_urlid ),\
-                        get_tags( url_or_urlid ),\
+                        get_title( metadata ),\
+                        get_author( metadata ),\
+                        get_length( metadata ),\
+                        get_date( metadata ),\
+                        get_category( metadata ),\
+                        get_tags( metadata ),\
                         '0'\
-                        ) 
+                        )
     else:
-        raise ValueError( "Failed to store '" + url_or_urlid + "': did not validate" ) 
-        
-
-#subtitles =  str( get_subtitles( get_yt_sub_url( test_ids[0] ) ) )[2:-1]
-#parsed_subs = parse_raw_subtitles( subtitles ) 
-#DbTools.insert( 'test-title', 'test-urlid', parsed_subs['captions'], parsed_subs['timestamps'], '0') 
-
+        raise ValueError( "Failed to store '" + url_or_urlid + "': did not validate" )
