4c0b18feae79bfff6e6aa1d0c70039318b38ab29
Pulecz <pulec.mcm@gmail.com>
2017-01-31 19:27:26 +0100

    Passing category correctly, smarter verbose print

--- bot/reddit_robot.py
+++ bot/reddit_robot.py
@@ -10,21 +10,23 @@ see https://praw.readthedocs.io/en/latest/getting_started/authentication.html"""
 
 #TODO
 #return clean links
-#pass category correctly
 #since save_links overwrites the file, print some kind of diff
 #links such as these bellow have to be resolved, they don't have yt_id
+#work directly with DB,thus no more duplicate ids and links to reddit submission and video
 #    https://gaming.youtube.com/attribution_link?a=RLshOXtkiZI&u=%2Fuser%2FMadZombiie%2Flive
 
 reddit = praw.Reddit('vidpager_reader')
 reddit.read_only = True
 
+category = 'hot'
 max_limit = 300
 #max_limit = 981 #it won't get higher, prints by 100 anyway
-#with new, 5 is very high score
+#take only posts with upvotes above limit
+#mind that with category new, 5 is very high score
 score_limit = 1
+target_file = 'links'
 yt_domain1 = 'youtube.com'
 yt_domain2 = 'youtu.be'
-target_file = 'links'
 verbose = True
 wait_time = 300
 
@@ -43,20 +45,41 @@ def return_rdict(submission):
             'url' : submission.url
             })
 
-"""
-reddit.domain categories:
+def return_category(conn, category='hot'):
+    """returns reddit object TODO which? with selected category
+    if no category matches, returns None
+    reddit.categories:
     controversial
     hot
     new - full of all kinds of bull****
-    rising - didn't work
+    rising - didn't work, not implemented
     random_rising
-    top
-"""
+    top"""
+    if category == 'controversial':
+        conn = conn.controversial(limit=max_limit)
+        return conn
+    if category == 'hot':
+        conn = conn.hot(limit=max_limit)
+        return conn
+    if category == 'new':
+        conn = conn.new(limit=max_limi)
+        return conn
+    if category == 'random_rising':
+        conn = conn.random_rising(limit=max_limit)
+        return conn
+    if category == 'top':
+        conn = conn.top(limit=max_limit)
+        return conn
 
 
 def get_domain(domain, fk = 0, filter_by_score=False, category = 'hot'):
+    conn = reddit.domain(domain)
+    conn = return_category(conn, category)
+    if conn is None:
+        print('Category',category, 'not supported')
+        return False
     result = {}
-    for s in reddit.domain(domain).new(limit=max_limit):
+    for s in conn:
         if filter_by_score:
             if s.score >= score_limit:
                 result[fk] = return_rdict(s)
@@ -71,8 +94,12 @@ def get_subreddit(subreddits, fk = 0, filter_by_score=False, category = 'hot'):
     target_subreddit = ''
     for sub in subreddits.split(','):
         target_subreddit += '+' + sub
-    target_subreddit[1:]
-    for s in reddit.subreddit(target_subreddit[1:]).new(limit=max_limit):
+    conn = reddit.subreddit(target_subreddit[1:])
+    conn = return_category(conn, category)
+    if conn is None:
+        print('Category',category, 'not supported')
+        return False
+    for s in conn:
         if filter_by_score:
             if s.score >= score_limit:
                 result[fk] = return_rdict(s)
@@ -113,22 +140,24 @@ def compare_dicts(d1,d2):
     return result
 
 
-
-def save_links(dict, verbose=True):
+def print_links(dict):
     stuff = {}
     for i in dict:
          stuff[i] = bunch[i]['rscore']
     sorted_by_lowest_score = sorted(stuff.items(), key=operator.itemgetter(1))
-    if verbose:
-        for index in sorted_by_lowest_score:
-            #index is tupe with key and rscore
-            #getting UnicodeError on Raspberry
-            print("{0}|{1}|<{2}>"
-            .format(index[1], dict[index[0]]['rtitle'],dict[index[0]]['url']))
+    for index in sorted_by_lowest_score:
+        #index is tupe with key and rscore
+        #getting UnicodeError on Raspberry
+        print("{0}|{1}|<{2}>"
+        .format(index[1], dict[index[0]]['rtitle'],dict[index[0]]['url']))
+
+
+def save_links(dict):
     with open(target_file,'w') as f:
         for key, value in dict.items():
             f.write(dict[key]['url']+'\n')
     print('Saved {0} links'.format(len(dict)))
+
 
 if __name__ == '__main__':
     def end_loop():
@@ -141,15 +170,21 @@ if __name__ == '__main__':
         print('Collecting new bunch')
         try:
             new_bunch = {}
-            new_bunch_yt1 = get_domain(yt_domain1, fk = 0, filter_by_score=True)
+            new_bunch_yt1 = get_domain(yt_domain1, fk = 0,
+                filter_by_score=True, category = category)
+
             print('Got {0} items from {1}'.format(len(new_bunch_yt1), yt_domain1))
-            new_bunch.update(new_bunch_yt1)
-            new_bunch_yt2 = get_domain(yt_domain2, fk = len(new_bunch_yt1), filter_by_score=True)
+            new_bunch.update(new_bunch_yt1) #add new stuff
+            new_bunch_yt2 = get_domain(yt_domain2, fk = len(new_bunch_yt1),
+                filter_by_score=True, category = category)
+
             print('Got {0} items from {1}'.format(len(new_bunch_yt2), yt_domain2))
-            new_bunch.update(new_bunch_yt2)
-            new_bunch_subs = get_subreddit('videos,documentaries', fk = len(new_bunch), filter_by_score=True)
+            new_bunch.update(new_bunch_yt2) #add new stuff
+            new_bunch_subs = get_subreddit('videos,documentaries', fk = len(new_bunch),
+                filter_by_score=True, category = category)
+
             print('Got {0} items from {1}'.format(len(new_bunch_subs), 'videos and documentaries'))
-            new_bunch.update(new_bunch_subs)
+            new_bunch.update(new_bunch_subs) #update
         except RequestException as e:
             print('\n\nCAUGHT THE ERROR', e, '\n\n')
             continue
@@ -170,7 +205,10 @@ if __name__ == '__main__':
                 #TODO this results mess for now
                 #bunch = compare_dicts(bunch,new_bunch)
                 pass
-        save_links(bunch,verbose=verbose)
+        save_links(bunch)
+        if verbose:
+            time.sleep(5) #give 5s to read progress before link spam
+            print_links(bunch)
         print("Sleeping for {0}m".format(round(wait_time/60)))
         end_loop()
         time.sleep(wait_time)
